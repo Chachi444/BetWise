00eee6a4ee3d95226ac606534eb72f73fdae4bc7
Charity Chinemerem Okechukwu <okechukwucharitychinemerem@gmail.com>
2025-06-25 08:56:53 +0100

    Wip

--- betwise-backend/Routes/index.js
+++ betwise-backend/Routes/index.js
@@ -1,10 +1,24 @@
-const authRoutes = require("./authRoutes")
+// const express = require("express")
+// const router = express.Router();
+
+
+// const authRoutes = require("./authRoutes")
 
 
 
-const routes = [
-    authRoutes
-]
+// const routes = [
+//     authRoutes
+// ]
 
 
-module.exports = routes
+// module.exports = router
+
+const express = require("express");
+const router = express.Router();
+
+const authRoutes = require("./authRoutes");
+
+// Mount all auth routes at the root path
+router.use("/", authRoutes);
+
+module.exports = router;
--- betwise-backend/index.js
+++ betwise-backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-dotenv.config(); // <-- Make sure this is called right after requiring dotenv
+dotenv.config(); 
 const Auth = require("./Models/authModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
